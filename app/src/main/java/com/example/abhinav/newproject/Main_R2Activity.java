64043b8a14d5ca51a8e55595a0606a22058ab9d8
DeepikaIngavale <deepikaingavale@gmail.com>
2019-02-28 10:24:45 +0530

    UI changes

--- app/src/main/java/com/example/abhinav/newproject/Main_R2Activity.java
+++ app/src/main/java/com/example/abhinav/newproject/Main_R2Activity.java
@@ -1,10 +1,9 @@
 package com.example.abhinav.newproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 
